e93d390d961a1ff146912caf477a185076c9949b
unknown <bruteakasylo@gmail.com>
2021-09-29 23:07:36 +0200

    first commit

---
+++ cliMenuBase/ConsoleHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cliMenuBase
+{
+    public class ConsoleHelper
+    {
+        public static int MultipleChoice(bool canCancel, params string[] options)
+        {
+            const int startX = 1;
+            const int startY = 0;
+            const int optionsPerLine = 1;
+            const int spacingPerLine = 14;
+            int currentSelection = 0;
+            ConsoleKey key;
+            Console.CursorVisible = false;
+
+            do
+            {
+                Console.Clear();
+                for (int i = 0; i < options.Length; i++)
+                {
+                    Console.SetCursorPosition(startX + (i % optionsPerLine) * spacingPerLine, startY + i / optionsPerLine);
+                    if (i == currentSelection)
+                        Console.Write("> ");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.Write(options[i]);
+                    Console.ResetColor();
+                }
+                key = Console.ReadKey(true).Key;
+                switch (key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        {
+                            if (currentSelection % optionsPerLine > 0)
+                                currentSelection--;
+                            break;
+                        }
+                    case ConsoleKey.RightArrow:
+                        {
+                            if (currentSelection % optionsPerLine < optionsPerLine - 1)
+                                currentSelection++;
+                            break;
+                        }
+                    case ConsoleKey.UpArrow:
+                        {
+                            if (currentSelection >= optionsPerLine)
+                                currentSelection -= optionsPerLine;
+                            break;
+                        }
+                    case ConsoleKey.DownArrow:
+                        {
+                            if (currentSelection + optionsPerLine < options.Length)
+                                currentSelection += optionsPerLine;
+                            break;
+                        }
+                    case ConsoleKey.Escape:
+                        {
+                            if (canCancel)
+                                return -1;
+                            break;
+                        }
+                }
+            }
+            while (key != ConsoleKey.Enter);
+            Console.CursorVisible = true;
+            return currentSelection;
+        }
+    }
+}
---
+++ cliMenuBase/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Console = Colorful.Console;
+using System.Threading;
+using System.Diagnostics;
+
+namespace cliMenuBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        startOfProgram:
+            Console.Title = "cliMenuBase";
+
+            //Console.WindowHeight = 8; Change console height after adding all options to make the console the same size as the menu
+            //Console.WindowWidth = 25; Change console width after adding all options to make the console the same size as the menu
+
+            Random rnd = new Random();
+
+            Process Proc = new Process();
+
+            int sleepTimeout = rnd.Next(1, 501);
+            int selectedClass = ConsoleHelper.MultipleChoice(true, "Option 1", "Option 2", "Option 3");
+
+            if(selectedClass == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Starting Option 1!", Color.Magenta);
+                Thread.Sleep(sleepTimeout);
+                Process process = Process.Start(@"Path to option 1");
+                int id = process.Id;
+                Process tempProc = Process.GetProcessById(id);
+                Console.WriteLine("Started!", Color.Magenta);
+                Console.Write("Going back in ", Color.Magenta);
+                for (int a = 4; a >= 0; a--)
+                {
+                    Console.CursorLeft = 14;
+                    int b = a + 1;
+                    Console.Write("{0} ", b, Color.Magenta);    // Add space to make sure to override previous contents
+                    System.Threading.Thread.Sleep(1000);
+                }
+                goto startOfProgram;
+            }
+            else if (selectedClass == 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Starting Option 2", Color.Magenta);
+                Thread.Sleep(sleepTimeout);
+                Process process = Process.Start(@"Path to option 2");
+                int id = process.Id;
+                Process tempProc = Process.GetProcessById(id);
+                Console.WriteLine("Started!", Color.Magenta);
+                Console.Write("Going back in ", Color.Magenta);
+                for (int a = 4; a >= 0; a--)
+                {
+                    Console.CursorLeft = 14;
+                    int b = a + 1;
+                    Console.Write("{0} ", b, Color.Magenta);    // Add space to make sure to override previous contents
+                    System.Threading.Thread.Sleep(1000);
+                }
+                goto startOfProgram;
+            }
+            else if (selectedClass == 2)
+            {
+                Console.Clear();
+                Console.WriteLine("Starting Option 3", Color.Magenta);
+                Thread.Sleep(sleepTimeout);
+                Process process = Process.Start(@"Path to option 3");
+                int id = process.Id;
+                Process tempProc = Process.GetProcessById(id);
+                Console.WriteLine("Started!", Color.Magenta);
+                Console.Write("Going back in ", Color.Magenta);
+                for (int a = 4; a >= 0; a--)
+                {
+                    Console.CursorLeft = 14;
+                    int b = a + 1;
+                    Console.Write("{0} ", b, Color.Magenta);    // Add space to make sure to override previous contents
+                    System.Threading.Thread.Sleep(1000);
+                }
+                goto startOfProgram;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid Option", Color.Magenta);
+                Thread.Sleep(sleepTimeout);
+                Console.Write("Going back in ", Color.Magenta);
+                for (int a = 4; a >= 0; a--)
+                {
+                    Console.CursorLeft = 14;
+                    int b = a + 1;
+                    Console.Write("{0} ", b, Color.Magenta);    // Add space to make sure to override previous contents
+                    System.Threading.Thread.Sleep(1000);
+                }
+                goto startOfProgram;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
